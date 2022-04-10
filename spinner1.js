b133a22dfdbc1ffb11f2b2473ba16e82283bd8c5
Yoyo Yang <yoyo.truly@gmail.com>
2022-04-09 21:37:12 -0400

    Add basic implementation of spinner animation in CLI

---
+++ spinner1.js
@@ -0,0 +1,5 @@
+setTimeout(() => process.stdout.write("\r|  "), 100);
+setTimeout(() => process.stdout.write("\r/  "), 300);
+setTimeout(() => process.stdout.write("\r-  "), 500);
+setTimeout(() => process.stdout.write("\r\\  "), 700);
+setTimeout(() => process.stdout.write("\r|  "), 900);
